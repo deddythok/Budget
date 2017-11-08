39acf828fbbb1cc2e8dd621f741c3c79aa413296
deddy <32954228+deddythok@users.noreply.github.com>
2017-11-08 16:57:36 +0700

    added new function

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-var budgetController = (function() {
-	var x = 23;
+// var budgetController = (function() {
+// 	var x = 23;
 
-	var add = function(a) {
-		return x + a;
+// 	var add = function(a) {
+// 		return x + a;
+// 	}
+
+// 	return {
+// 		publicTest: function(b) {
+// 			console.log(add(b));
+// 		}
+// 	}
+// })();
+
+var fungsiUtama = (function() {
+	var a = 1;
+
+	var tambah = function(x) {
+		return a + x;
 	}
-
 	return {
 		publicTest: function(b) {
-			console.log(add(b));
+			console.log(tambah(b));
 		}
 	}
 })();
+
