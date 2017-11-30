f19156fe1e1c267b5e1eb5ef3f0b0ebfc3ec31bd
Deddy thok <deddy.donchild@gmail.com>
2017-11-30 21:32:31 +0700

    small fixes

--- app.js
+++ app.js
@@ -131,11 +131,11 @@ var UIController = (function() {
 
 			if (type === 'inc') {
 			element = DOMstrings.incomeContainer;
-            html =  '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            html =  '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
         	} else if (type === 'exp'){
         	element = DOMstrings.expenseContainer;
 
-            html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
         	}
 			//Replace placeholder text with some actual data
 			newHtml = html.replace('%id%', obj.id);
@@ -228,8 +228,23 @@ var controller = (function(budgetCtrl,UICtrl) {
 	};
 
 	var ctrlDeleteItem = function(event) {
-		console.log(event.target);
-	};
+		var itemID, splitID, type, ID;
+		itemID = (event.target.parentNode.parentNode.parentNode.parentNode.id);
+
+		if(itemID) {
+			//inc-1
+			splitID = itemID.split('-');
+			type = splitID[0];
+			ID = splitID[1];
+
+			//1. delte the item from the data structure
+
+			//2. Delete the item from the UI
+
+
+			//3. Update and show the new budget
+		}
+	};	
 
 
 	return {
